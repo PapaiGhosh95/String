6e8b47a56452110697fa69d5e3a2fef252761c96
PapaiGhosh95 <74486016+PapaiGhosh95@users.noreply.github.com>
2020-11-15 13:21:10 +0530

    Update Compress String.cpp
    Description added for the question.

--- Compress String.cpp
+++ Compress String.cpp
@@ -1,3 +1,20 @@
+/*
+Write a program to do basic string compression. For a character which is consecutively repeated more than once, 
+replace consecutive duplicate occurrences with the count of repetitions.
+
+Exmple:
+If a string has 'x' repeated 5 times, replace this "xxxxx" with "x5".
+The string is compressed only when the repeated character count is more than 1.
+
+Sample Input 1:
+aaabbccdsa
+Sample Output 1:
+a3b2c2dsa
+
+
+*/
+
+
 void stringCompression(char input[]) {
     // Write your code here
     int len=0;
